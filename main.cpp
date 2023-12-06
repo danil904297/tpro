de73f3f2025f844d8f019a13ba7307563f988a1b
qweqwe <114856755+danil904297@users.noreply.github.com>
2023-12-07 00:34:25 +0300

    Сделал меню
    Сделал меню

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "SFML/Graphics.hpp"
+#include "Menu.h"
+
+
+using namespace sf;
+int main()
+{
+
+    // Создание окна игры
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Трон");
+    menu(window);
+
+
+
+}
