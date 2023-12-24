67f875901ac1b3258829bd04c2d158e13677a9b6
22350306 <shatlgleb@gmail.com>
2023-12-24 15:37:23 +0300

    Добавлен файл  с настройками

---
+++ settings.h
@@ -0,0 +1,18 @@
+#pragma once
+const int WIDTH = 2520;
+const int HEIGHT = 1680;
+
+const float PLAYER_SPEED = 4.0;
+
+const int PLAYER_LENGTH = 20;
+const int PLAYER_HEIGHT = 10;
+
+const int TEXT_SIZE = 50;
+
+enum directions
+{
+	up,
+	right,
+	down,
+	left
+};
